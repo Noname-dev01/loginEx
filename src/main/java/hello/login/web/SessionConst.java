93ee8ca02a80d4544b998dac6b1f5740b7af7231
noname_dev <noname_dev@naver.com>
2021-12-15 16:32:36 +0900

    Initial commit

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,11 @@
+package hello.login.web;
+
+//보통 이렇게 사용한다
+//public interface SessionConst {
+//    String LOGIN_MEMBER = "loginMember";
+//}
+
+//강의에서는 아직 어려운 사람들을 위해 아래처럼 사용했다.
+public class SessionConst {
+    public static final String LOGIN_MEMBER = "loginMember";
+}
